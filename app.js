de5fa1f53c0d6a870b255fb7dc05cca9f9c25e65
Maxime Gaillard <maxgaillard@gmail.com>
2022-05-17 20:46:47 -0400

    types

---
+++ app.js
@@ -0,0 +1,4 @@
+var add = function (n1, n2) {
+    return n1 + n2;
+};
+console.log(add(+'5', 5));
---
+++ app.ts
@@ -0,0 +1,12 @@
+const add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+// There are three types in typescript
+// number
+// string
+// boolean
+// Types are assigned during compilation
+// Whereas types are infer during runtime in javascript
+
+console.log(add(5, 5));
