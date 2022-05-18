26fcff89ad33c3574c327a7ae2516aebbf2b566c
Maxime Gaillard <maxgaillard@gmail.com>
2022-05-17 21:12:51 -0400

    Arrays in typescript

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var person = {
     name: 'Maxime Gaillard',
-    age: 45
+    age: 45,
+    hobbies: ['Coding', 'Biking']
 };
-console.log(person);
+// Define an array of strings
+var favoriteActivities;
+// Type error, 5 is number
+favoriteActivities = ['Coding', 5];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+var anyThing;
+anyThing = [0, 'Hello', false];
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -3,7 +3,20 @@ const person: {
   age: number;
 } = {
   name: 'Maxime Gaillard',
-  age: 45
+  age: 45,
+  hobbies: ['Coding', 'Biking']
 };
+
+// Define an array of strings
+let favoriteActivities: string[];
+// Type error, 5 is number
+favoriteActivities = ['Coding', 5];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+let anyThing: any[];
+anyThing = [0, 'Hello', false];
 
 console.log(person.name);
